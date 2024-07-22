9f841a6f162e767f4ae73fd46e0570e76b47f1ae
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-22 20:31:49 +0000

    Update 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,8 @@
 export default function getNeighborhoodsList()  {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
-  const addNeighborhood = (neighborhood) => {
-    self.sanFranciscoNeighborhoods.push(neighborhood);
-    return self.sanFranciscoNeighborhoods;
-  };
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  }
 }
-
