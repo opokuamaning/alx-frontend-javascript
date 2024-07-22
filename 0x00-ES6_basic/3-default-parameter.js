20ef1092de94afa0d21904a01b81fadffa31e1b4
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-22 21:01:53 +0000

    Update 3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
 export default function getSumOfHoods(initialNumber, exapnsion1989 = 89, expansion2019 = 19) {
-  // eslint-disable-next-line linebreak-style
   return initialNumber + exapnsion1989 + expansion2019;
-// eslint-disable-next-line linebreak-style
 }
