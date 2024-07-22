3d84e8d385b22e5ffecd1538e55593ab60a1b227
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-22 23:20:32 +0000

    Update 10-loop.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,10 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/extensions
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
+    // eslint-disable-next-line no-param-reassign
     array[idx] = appendString + idx;
   }
 
