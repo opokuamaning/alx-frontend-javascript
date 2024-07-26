7c907bdc6cfe7620cf27cb3aafadb4b59dd00cc9
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 20:26:22 +0000

    Added 7-load_balancer.js

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+
+export default function loadBalancer(chinaDownload, USDownload) {
+    return Promise.race([chinaDownload, USDownload]).then((value){
+        return value;
+    });
+}
