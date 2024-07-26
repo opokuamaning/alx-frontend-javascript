355ffeb5bd6655fd8b1190ac8fae5218fad4d1b8
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 16:05:27 +0000

    Update 5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 
 export default function uploadPhoto(filename) {
     return Promise.reject(new Error(`${filename} cannot be processed`));
