313907eb8f55242cf8e63151aa628fc98ab215b7
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 00:25:59 +0000

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,11 @@
-export default function getResponseFromAPI(){
-    return new Promise((resolve, reject) => {
-        if (true){
-            resolve();
-        }else{
-            reject();
-        }
-    })
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
