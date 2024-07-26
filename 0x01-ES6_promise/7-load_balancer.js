f3725cce4e53cf05691e0c09034d2c88ce47e3e4
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 20:34:43 +0000

    Update 7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -3,7 +3,7 @@
 /* eslint-disable linebreak-style */
 
 export default function loadBalancer(chinaDownload, USDownload) {
-    return Promise.race([chinaDownload, USDownload]).then((value){
+    return Promise.race([chinaDownload, USDownload]).then(value => {
         return value;
     });
 }
