fada759bc821c25eb2bc2aeec74bde5f58bba148
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 23:33:28 +0000

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ 
-        status: 200, 
-        body: "success" 
+    .then(() => ({
+      status: 200,
+      body: 'success',
     }))
     .catch(() => Error())
     .finally(() => {
-      console.log("Got a response from the API");
+      console.log('Got a response from the API');
     });
 }
