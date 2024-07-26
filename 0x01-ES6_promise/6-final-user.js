3f0fcbdf1c6fd0e9c8e3ecb580b5c04bce2e2151
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 20:18:03 +0000

    Update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,20 +2,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
 
-import { signUpUser } from "./6-user-promise.js";
-import { uploadPhoto } from "./5-photo-reject.js";
+import  signUpUser  from "./4-user-promise.js";
+import  uploadPhoto  from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.all([promise1, promise2])
-    .then((values) => {
-      console.log(
-        `${values[0].firstName} ${values[0].lastName} ${values[1].body}`
-      );
-    })
-    .catch(() => {
-      console.log("Signup system offline");
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((res) => {
+    const results = [];
+    res.forEach((result) => {
+        if(result.status === 'fulfilled') {
+            results.push({ status: result.status, value: result.value });
+        } else {
+            results.push({ status: result.status, value: result.reason.message });
+        }
     });
+    return results;
+  });
 }
