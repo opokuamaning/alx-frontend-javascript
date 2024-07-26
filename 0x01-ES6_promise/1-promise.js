803e966fac6d481197877b0a7864fd2ffb15754d
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 14:03:54 +0000

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
 export default function getFullResponseFromAPI(success) {
-  const API = { status: 200, body: "Success" };
-  const APIError = new Error("The fake API is not working currently");
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve(API);
+      resolve({ 
+        status: 200, 
+        body: "Success" 
+    });
     } else {
-      reject(new Error(APIError));
+      reject(new Error("The fake API is not working currently"));
     }
   });
 }
