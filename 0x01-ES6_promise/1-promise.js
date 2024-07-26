88641df85e1505f9ffed96521ca605ef511ec257
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 14:24:01 +0000

    Update 2-then.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,16 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable linebreak-style */
-/* eslint-disable semi */
-// eslint-disable-next-line import/extensions
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ 
-        status: 200, 
-        body: "Success"
+/* eslint-disable no-console */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: "success" }))
+    .catch(() => Error())
+    .finally(() => {
+      console.log("Got a response from the API");
     });
-    } else {
-      reject(new Error("The fake API is not working currently"));
-    }
-  });
 }
