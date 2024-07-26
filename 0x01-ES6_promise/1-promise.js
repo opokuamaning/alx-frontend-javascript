0ae487ff5413e06e5846889ad8378d93d50b7206
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 09:36:36 +0000

    Added 1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success){
+    const API = { status: 200, body: "Success" };
+    const APIError = new Error('The fake API is not working currently');
+    return new Promise((resolve, reject) => {
+        if(success){
+            resolve(API);
+        } else {
+            reject(new Error(APIError));
+        }
+    });
+}
