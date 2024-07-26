dfa8ee1a86074babb745a7228f6c0b3f71d64d4e
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 23:40:25 +0000

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
 
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpUserPromise = signUpUser(firstName, lastName);
@@ -13,13 +13,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     (values) => {
       const result = [];
       values.forEach((element) => {
-        if (element.status === "fulfilled") {
+        if (element.status === 'fulfilled') {
           result.push({ status: element.status, value: element.value });
         } else {
           result.push({ status: element.status, value: `${element.reason}` });
         }
       });
       return result;
-    }
+    },
   );
 }
