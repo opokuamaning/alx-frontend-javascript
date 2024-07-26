22f2c58d559d21ed2cb260d79a17a39c6a70264a
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 14:41:20 +0000

    new commit

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-console */
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: "success" }))
+    .then(() => ({ 
+        status: 200, 
+        body: "success" 
+    }))
     .catch(() => Error())
     .finally(() => {
       console.log("Got a response from the API");
