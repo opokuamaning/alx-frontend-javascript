19230697a1e33de57ad47dcb81a1b8879254248f
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-27 00:11:47 +0000

    new commit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom from './classroom';
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return newList;
 }
