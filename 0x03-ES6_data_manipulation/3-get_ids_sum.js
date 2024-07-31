2b5871be90948da3ce0669a4cd5f275a65391a76
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-31 01:11:23 +0000

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,3 @@
 export default function getStudentIdsSum(getStudent) {
-//   if (!Array.isArray(getStudent)) {
-//     return [];
-//   }
   return getStudent.reduce((sum, current) => sum + current, 0);
 }
